50c41d6d842c4b432ecf8935d278c7955455494b
Samarth Sharma <samarthsharma351@gmail.com>
2026-01-06 14:10:36 -0800

    chore: Adds drizzle and users schema.

---
+++ src/db/schemas/users.ts
@@ -0,0 +1,43 @@
+import {
+  boolean,
+  uniqueIndex,
+  pgEnum,
+  bigserial,
+  text,
+  varchar,
+  timestamp,
+  pgTable,
+} from "drizzle-orm/pg-core";
+
+export const accountStatusEnum = pgEnum("account_status", [
+  "active",
+  "suspended",
+  "deactivated",
+  "shadowbanned",
+]);
+
+export const users = pgTable(
+  "users",
+  {
+    id: bigserial("id", { mode: "bigint" }).primaryKey(),
+    username: varchar("username", { length: 32 }).notNull().unique(),
+    name: varchar("name", { length: 32 }).notNull(),
+    email: varchar("email", { length: 64 }).notNull(),
+    passwordHash: text("password_hash"),
+    address: text("address"),
+    country: varchar("country", { length: 32 }),
+    isPrivate: boolean("is_private").default(false).notNull(),
+    isVerified: boolean("is_verified").default(false).notNull(),
+    bio: varchar("bio", { length: 240 }),
+    displayPictureUrl: text("display_picture_url"),
+    accountStatus: accountStatusEnum("account_status")
+      .default("active")
+      .notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
+  },
+  (table) => ({
+    usernameIdx: uniqueIndex("username_idx").on(table.username),
+    emailIdx: uniqueIndex("emaid_idx").on(table.email),
+  })
+);
