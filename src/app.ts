c6c0f848b0ce62a043c6cefadc0752025ccf69b5
Samarth Sharma <samarthsharma351@gmail.com>
2026-01-06 21:02:57 -0800

    chore: Uses complete names of variables.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { authRouter } from "./routes/auth.routes";
 import { allUsersRouter } from "./routes/all-users.routes";
 import { auth } from "./auth/jwt";
@@ -9,11 +9,14 @@ export function createApp() {
   const app = express();
   app.use(express.json());
 
-  app.get("/health", async (req, res, next) => {
-    return res.status(200).json({
-      ok: true,
-    });
-  });
+  app.get(
+    "/health",
+    async (request: Request, response: Response, next: NextFunction) => {
+      return response.status(200).json({
+        ok: true,
+      });
+    }
+  );
 
   app.use("/v1/auth", authRouter);
   app.use("/v1/posts", auth, postsRouter);
