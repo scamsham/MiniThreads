6ca92f0a67607e0deccf4e8bdbab8ff82733ef7c
Samarth Sharma <samarthsharma351@gmail.com>
2026-01-06 21:01:40 -0800

    chore: Adds empty schemas for posts and comments.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { authRouter } from "./routes/auth.routes";
 import { allUsersRouter } from "./routes/all-users.routes";
+import { auth } from "./auth/jwt";
+import { postsRouter } from "./routes/posts.routes";
+import { commentsRouter } from "./routes/comments.routes";
 
 export function createApp() {
   const app = express();
@@ -13,6 +16,10 @@ export function createApp() {
   });
 
   app.use("/v1/auth", authRouter);
+  app.use("/v1/posts", auth, postsRouter);
+  app.use("/v1/comments", auth, commentsRouter);
+
+  // seed user endpoint
   app.use("/all-users", allUsersRouter);
 
   return app;
