485e01627a8a7679278828caf5724aa4d0b21425
Samarth Sharma <samarthsharma351@gmail.com>
2026-01-19 20:33:44 -0800

    chore: Adds relationship permissions.

--- src/lib/privacy.ts
+++ src/lib/privacy.ts
@@ -1,9 +1,10 @@
-import { eq } from "drizzle-orm";
-import { usersTable } from "../db";
+import { and, eq } from "drizzle-orm";
+import { followsTable, usersTable } from "../db";
 import { db } from "../db/client";
 import { cacheGetOrSetJson } from "../infra/redis/cache";
 
 type PrivacyFlag = { isPrivate: boolean };
+type FollowFlag = { allowed: boolean };
 
 /*
 Note to self: Caching private content on cache is dangerous as it may lead to data leaks.
@@ -21,19 +22,40 @@ export async function canViewUserContent(
     privacyKey,
     60,
     async () => {
-      const [u] = await db
+      const [user] = await db
         .select({ isPrivate: usersTable.isPrivate })
         .from(usersTable)
         .where(eq(usersTable.id, ownerId))
         .limit(1);
 
-      return { isPrivate: u?.isPrivate ?? false };
+      return { isPrivate: user?.isPrivate ?? false };
     }
   );
 
   // for public accounts, we let everyone view
   if (!isAccountPrivate.isPrivate) return true;
 
-  // return defult deny for private accounts
-  return false;
+  // cache relationship between viewer and the owner
+  const followKey = `follow:${viewerId}:${ownerId}`;
+  const { value: follow } = await cacheGetOrSetJson<FollowFlag>(
+    followKey,
+    30,
+    async () => {
+      const followCheck = await db
+        .select({ followerId: followsTable.followerId })
+        .from(followsTable)
+        .where(
+          and(
+            eq(followsTable.followerId, viewerId),
+            eq(followsTable.followeeId, ownerId),
+            eq(followsTable.status, "accepted")
+          )
+        )
+        .limit(1);
+
+      return { allowed: followCheck.length > 0 ? true : false };
+    }
+  );
+
+  return follow.allowed;
 }
